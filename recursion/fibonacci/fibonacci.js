54a14f6c5d09261d2b6332dbe99e664aee90a570
Benjamin Toll <benjam72@yahoo.com>
2016-10-28 12:13:38 -0400

    Added fibonacci sequence

---
+++ recursion/fibonacci/fibonacci.js
@@ -0,0 +1,12 @@
+/* eslint-disable no-console */
+
+const fibonacci = n => {
+    if (n < 2) {
+        return n;
+    }
+
+    return fibonacci(n - 2) + fibonacci(n - 1);
+};
+
+console.log(fibonacci(75));
+
---
+++ recursion/fibonacci/memoized.js
@@ -0,0 +1,16 @@
+/* eslint-disable no-console */
+
+const fibonacci = (n, cache = {}) => {
+    if (n < 2) {
+        return n;
+    }
+
+    if (cache[n]) {
+        return cache[n];
+    } else {
+        return cache[n] = fibonacci(n - 2, cache) + fibonacci(n - 1, cache);
+    }
+};
+
+console.log(fibonacci(75));
+
