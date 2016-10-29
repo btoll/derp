297c2c1c7a089fd425438b65d27b3d134ff6d296
Benjamin Toll <benjam72@yahoo.com>
2016-10-29 12:11:59 -0400

    Added bitwise dir

---
+++ bitwise/xorSwap.js
@@ -0,0 +1,17 @@
+/* eslint-disable no-console */
+
+let n = 512;
+let k = 256;
+
+const swap = () => (
+    n = n ^ k,
+    k = n ^ k,
+    n = n ^ k
+);
+
+console.log(`(before swap) n: ${n}`, `k: ${k}`);
+swap();
+console.log(` (after swap) n: ${n}`, `k: ${k}`);
+swap();
+console.log(`      (reset) n: ${n}`, `k: ${k}`);
+
