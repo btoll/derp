239f5e4f9886c300e369e47dec7debc7efe00bc9
Benjamin Toll <benjam72@yahoo.com>
2016-12-23 12:34:15 -0500

    Added fizzbuzz

---
+++ algorithms/fizzbuzz.js
@@ -0,0 +1,29 @@
+/* eslint-disable no-console */
+
+for (let i = 0; i < 100; i++) {
+    let fizzbuzz = 0;
+
+    if (i % 3 === 0) {
+        fizzbuzz = 1;
+    }
+
+    if (i % 5 === 0) {
+        fizzbuzz += 2;
+    }
+
+    switch (fizzbuzz & 3) {
+        case 0:
+            console.log(i);
+            break;
+        case 1:
+            console.log('fizz');
+            break;
+        case 2:
+            console.log('buzz');
+            break;
+        case 3:
+            console.log('fizzbuzz');
+            break;
+    }
+}
+
