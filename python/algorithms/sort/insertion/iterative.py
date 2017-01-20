29a0372dea8d380a445b3612e137e73d0a6bf3c7
Benjamin Toll <benjam72@yahoo.com>
2017-01-20 11:39:43 -0500

    Removed semi-colons

--- python/algorithms/sort/insertion/iterative.py
+++ python/algorithms/sort/insertion/iterative.py
@@ -1,29 +1,29 @@
-import sys;
+import sys
 
 def insert(toSort, dropIndex):
-    previous = dropIndex - 1;
-    key = toSort[dropIndex];
+    previous = dropIndex - 1
+    key = toSort[dropIndex]
 
     while previous > -1 and key < toSort[previous]:
         # Shift right.
-        toSort[dropIndex] = toSort[previous];
+        toSort[dropIndex] = toSort[previous]
 
-        dropIndex -= 1;
-        previous -= 1;
+        dropIndex -= 1
+        previous -= 1
 
-    toSort[dropIndex] = key;
+    toSort[dropIndex] = key
 
 def insertionSort(toSort):
     for i in range(1, len(toSort)):
-        insert(toSort, i);
+        insert(toSort, i)
 
-    return toSort;
+    return toSort
 
 def main(argv):
-    toSort = [188, 9, 7, 1, 5, 7777, 4, 3, 1, -99];
-    print('initial sort order', toSort);
-    print('            sorted', insertionSort(toSort));
+    toSort = [188, 9, 7, 1, 5, 7777, 4, 3, 1, -99]
+    print('initial sort order', toSort)
+    print('            sorted', insertionSort(toSort))
 
 if __name__ == '__main__':
-    main(sys.argv[1:]);
+    main(sys.argv[1:])
 
