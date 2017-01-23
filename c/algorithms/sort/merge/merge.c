669b54a0dc4776a42dc46609fa853157e9e01429
Benjamin Toll <benjam72@yahoo.com>
2017-01-23 12:02:47 -0500

    Added C merge sort

---
+++ c/algorithms/sort/merge/merge.c
@@ -0,0 +1,57 @@
+#include <string.h>
+#include "../sortlib.h"
+
+int array[10] = { 188, 9, 7, 1, 5, 7777, 4, 3, 1, -99 };
+unsigned int short len = sizeof(array) / sizeof(int);
+
+void merge(unsigned int short p, unsigned int short q, unsigned int short r) {
+    int lowHalfLen = q + 1 - p;
+    int highHalfLen = r - q;
+
+    int lowHalf[lowHalfLen];
+    int highHalf[highHalfLen];
+
+    int i = 0, j = 0, k = p;
+
+    memcpy(lowHalf, array + p, lowHalfLen * sizeof(int));
+    memcpy(highHalf, array + q + 1, highHalfLen * sizeof(int));
+
+    while (i < lowHalfLen && j < highHalfLen) {
+        if (lowHalf[i] < highHalf[j]) {
+            array[k] = lowHalf[i];
+            i++;
+        } else {
+            array[k] = highHalf[j];
+            j++;
+        }
+
+        k += 1;
+    }
+
+    while (i < lowHalfLen) {
+        array[k++] = lowHalf[i++];
+    }
+
+    while (j < highHalfLen) {
+        array[k++] = highHalf[j++];
+    }
+}
+
+int mergeSort(int p, int r) {
+    if (p >= r) {
+        return 0;
+    }
+
+    int q = (p + r) / 2;
+
+    mergeSort(p, q);
+    mergeSort(q + 1, r);
+    merge(p, q, r);
+}
+
+void main(void) {
+    printArray("Before sort:", array, len);
+    mergeSort(0, len - 1);
+    printArray("After sort:", array, len);
+}
+
