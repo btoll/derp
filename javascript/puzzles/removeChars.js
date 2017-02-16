708dc0cb298f1724f25368f3c0bfbaafc2e53c36
Benjamin Toll <benjam72@yahoo.com>
2017-02-15 21:27:39 -0500

    Added js removeChars.js example to puzzles/

---
+++ javascript/puzzles/removeChars.js
@@ -0,0 +1,20 @@
+/* eslint-disable no-console */
+
+// Remove all vowels to produce the sentence 'Bttl f th Vwls: Hw vs. Grzny'.
+
+const removeChars = (s, toRemove) => {
+    const vowels = toRemove.split('').reduce(
+        (acc, curr) =>
+            (acc[curr] = 1, acc)
+        , {}
+    );
+
+    return s.split('').filter(
+        c => !vowels[c]
+    ).join('');
+};
+
+console.log(
+    removeChars('Battle of the Vowels: Hawaii vs. Grozny', 'aeiou')
+);
+
